4f623bd1be6ddef8982cb70b095b43a696d70e48
ash <contato@ismaelnascimento.com>
2021-04-03 14:25:29 -0300

    [ADDED] Cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { mockData } = require("./mockData");
+
 const app = express();
 const port = process.env.port || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API Works!");
